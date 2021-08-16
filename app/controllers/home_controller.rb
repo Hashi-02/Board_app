63dcb8046dd2913f70bdd04d9bf66eb9c3a180c2
hassie <hassie0209@gmail.com>
2021-08-16 14:11:41 +0900

    CSSとか

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def top
+      @posts = Board.all.order(created_at: :desc)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  
   def index
     @posts = Board.all.order(created_at: :desc)
   end
